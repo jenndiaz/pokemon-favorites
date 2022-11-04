e3b7ea2e74ceeb3755d810b6ad3c6a9107d62efa
jenndiaz <jennifergrenierdiaz@gmail.com>
2022-11-04 14:30:16 -0600

    fetchs all pokemon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import React, { Component } from 'react';
 import logo from './pokemonLogo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="app-logo" alt="logo" />
-      </header>
-    </div>
-  );
+class App extends Component  {
+
+  state = { 
+    pokemonList: [],
+  }
+
+  componentDidMount(){
+    console.log('mounted')
+    this.getPokemon()
+  }
+
+  getPokemon = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+      .then(response => response.json())
+      .then(pokemon => this.setState({
+        pokemonList: pokemon
+      }))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="app-logo" alt="logo" />
+        </header>
+        <main>
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App;
