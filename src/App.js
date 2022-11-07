853106ca8f63d385feac8addaceab3d5d87b9556
jenndiaz <jennifergrenierdiaz@gmail.com>
2022-11-07 13:19:30 -0700

    refactor to use hooks

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './pokemonLogo.png';
 import PokemonCardContainer from './components/PokemonCardContainer';
 import './App.css';
 
-class App extends Component  {
+function App ()  {
 
-  state = { 
-    pokemonList: [],
-  }
+  const [pokemonList, setPokemonList] = useState([])
 
-  componentDidMount(){
-    this.getPokemonList()
-  }
-
-  getPokemonList = () => {
+  useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
-      .then(response => response.json())
-      .then(pokemon => this.setState({
-          pokemonList: pokemon.results
-        })
-      )
-  }
+    .then(response => response.json())
+    .then(pokemon => setPokemonList(pokemon.results)
+    )
+  })
 
-  render(){
+  if(pokemonList){
     return (
       <div className="App">
         <header className="header">
           <img src={logo} className="header__logo" alt="Pokemon logo" />
         </header>
         <main>
-          <PokemonCardContainer pokemonList={this.state.pokemonList} />
+          <PokemonCardContainer pokemonList={pokemonList} />
         </main>
       </div>
-    )
+  )
+  } else{
+    <p>Collecting Pokemon!</p>
   }
+
+
 }
 
 export default App;
