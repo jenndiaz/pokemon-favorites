96b161cc20ec7fc069ac3440683004debfe57fac
jenndiaz <jennifergrenierdiaz@gmail.com>
2022-11-07 13:19:16 -0700

    adds sorting logic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,47 +1,36 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import heart from '../heart-icon.svg'
 import heartOutline from '../heart-line-icon.svg'
 
 
-export default class Card extends Component {
+function Card ({pokemon}) {
 
-  state = {
-    data: [],
-    favorite: false
+  const [data, setData] = useState([])
+  const [favorite, setFavorite] = useState(false)
+
+  useEffect(() => {
+    fetch(pokemon.url)
+    .then(response => response.json())
+    .then(data => setData(data))
+  })
+
+
+  if(data.length === 0){
+    return( <p>Loading!</p> )
+  } else {
+  return (
+    <div key={pokemon.name}>
+      <h2>{pokemon.name}</h2>
+      <p>{data.weight}</p>
+      <img src={data.sprites.front_default} />
+      <button type='button' className='btn' onClick={() => setFavorite(!favorite)}>
+        <img className="btn__icon btn__icon-inactive" src={favorite ? heart : heartOutline} />
+      </button>
+      
+    </div>
+  )
   }
 
-  componentDidMount(){
-    this.getPokemonData()
-  }
-
-  getPokemonData = () => {
-    fetch(this.props.pokemon.url)
-      .then(response => response.json())
-      .then(data => this.setState({
-        data
-      }))
-  }
-
-
-  handleClick(){
-    this.setState({favorite: !this.state.favorite})
-  }
-
-  render() {
-    if(this.state.data.length === 0){
-      return( <p>Loading!</p> )
-    } else {
-    return (
-      <div key={this.props.pokemon.name}>
-        <h2>{this.props.pokemon.name}</h2>
-        <p>{this.state.data.weight}</p>
-        <img src={this.state.data.sprites.front_default} />
-        <button type='button' className='btn' onClick={() => this.handleClick()}>
-          <img className="btn__icon btn__icon-inactive" src={this.state.favorite ? heart : heartOutline} />
-        </button>
-        
-      </div>
-    )
-    }
-  }
 }
+
+export default Card;
--- src/components/PokemonCardContainer.jsx
+++ src/components/PokemonCardContainer.jsx
@@ -4,10 +4,9 @@ import Card from './Card'
 export default function PokemonCardContainer({pokemonList}) {
 
   const [sortOrder, setSortOrder] = useState('ASC')
-
-  const displayPokemon = () => pokemonList.map(pokemon => ( <Card pokemon={pokemon}/>))
   const handleSortOnClick = () => sortOrder === 'ASC' ? setSortOrder('DESC') : setSortOrder('ASC')
-
+  const sortedPokemonList = sortOrder === 'ASC' ? [...pokemonList].sort((a,b) => a.name > b.name ? 1 : -1) : [...pokemonList].sort((a,b) => a.name > b.name ? -1 : 1)
+  const displayPokemon = () => sortedPokemonList.map(pokemon => ( <Card pokemon={pokemon}/>))
 
     return (
       <>
