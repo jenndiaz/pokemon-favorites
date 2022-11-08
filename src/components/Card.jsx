57fccb4337e45f19affe07b4b472f4583a95b649
jenndiaz <jennifergrenierdiaz@gmail.com>
2022-11-08 08:54:42 -0700

    move favorite handling to parent component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,9 @@ import React, { useEffect, useState } from 'react'
 import heart from '../heart-icon.svg'
 import heartOutline from '../heart-line-icon.svg'
 
-
-function Card ({pokemon}) {
+function Card ({handleFavorites, favorites, pokemon}) {
 
   const [data, setData] = useState([])
-  const [favorite, setFavorite] = useState(false)
 
   useEffect(() => {
     let isActive = true
@@ -21,24 +19,24 @@ function Card ({pokemon}) {
     return () => {
       isActive = false
     }
-  }, [pokemon.url])
-
+  }, [pokemon])
 
   if(data.length === 0){
-    return( <p>Loading!</p> )
-  } else {
-  return (
     <div className='card' key={pokemon.name}>
-      <h2>{pokemon.name}</h2>
-      <img src={data.sprites.front_default} alt={`drawing of ${pokemon.name} sprite`} />
-      <p>{`weight: ${data.weight}`}</p>
-      <button type='button' className='btn' aria-label="Favorite" aria-pressed={favorite ? true : false} onClick={() => setFavorite(!favorite)}>
-        <img className='btn__icon' alt="" src={favorite ? heart : heartOutline} />
-      </button>
+      <h2>Loading Card...</h2>
     </div>
-  )
+  } else {
+    return (
+      <div className='card' key={pokemon.name}>
+        <h2>{pokemon.name}</h2>
+        <img  className='card__image' src={data.sprites.front_default} alt={`drawing of ${pokemon.name} sprite`} />
+        <p className='card__text'>{`weight: ${data.weight}`}</p>
+        <button type='button' className='btn' aria-label="Favorite" aria-pressed={favorites.includes(pokemon.name) ? true : false} onClick={() => handleFavorites(pokemon.name)}>
+          <img className='btn__icon' alt="" src={favorites.includes(pokemon.name) ? heart : heartOutline} />
+        </button>
+      </div>
+    )
   }
-
 }
 
 export default Card;
--- src/components/PokemonCardContainer.jsx
+++ src/components/PokemonCardContainer.jsx
@@ -4,14 +4,36 @@ import Card from './Card'
 export default function PokemonCardContainer({pokemonList}) {
 
   const [sortOrder, setSortOrder] = useState('ASC')
+  const [favorites, setFavorites] = useState([])
+
+
+  const handleFavorites = (name) => {
+    if(favorites.includes(name)){
+      setFavorites(favorites.filter(removal => removal !== name ))
+    } else {
+      setFavorites([...favorites, name])
+    }
+  } 
   const handleSortOnClick = () => sortOrder === 'ASC' ? setSortOrder('DESC') : setSortOrder('ASC')
   const sortedPokemonList = sortOrder === 'ASC' ? [...pokemonList].sort((a,b) => a.name > b.name ? 1 : -1) : [...pokemonList].sort((a,b) => a.name > b.name ? -1 : 1)
-  const displayPokemon = () => sortedPokemonList.map(pokemon => ( <Card pokemon={pokemon}/>))
+  const displayPokemon = () => sortedPokemonList.map(pokemon => ( 
+    <Card 
+      key={pokemon.name}
+      handleFavorites ={handleFavorites} 
+      favorites={favorites}
+      pokemon={pokemon}
+    />
+  ))
+
 
     return (
       <>
-        <div>
-          <button type='button' className='btn' onClick={handleSortOnClick}>Sort</button>
+        <div className='sort-container'>
+          <label>Sort By:</label>
+          <select className='btn' name='sort' id='sort' onChange={handleSortOnClick}>
+            <option value='ASC' defaultValue>Alphabetical A-Z</option>
+            <option value='DESC'>Alphabetical Z-A</option>
+          </select> 
         </div>
         <div className='card-container'>{displayPokemon()}</div>
       </>
